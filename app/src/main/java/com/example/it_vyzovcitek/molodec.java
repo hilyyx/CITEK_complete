a7b37339a908bd57b10b96b5c43b25f67422fd4f
egorka-19 <suyakovegr@gmail.com>
2024-11-27 17:13:57 +0400

    -features оценивание результатов

--- app/src/main/java/com/example/it_vyzovcitek/molodec.java
+++ app/src/main/java/com/example/it_vyzovcitek/molodec.java
@@ -15,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class molodec extends AppCompatActivity {
     private int scoree;
     private String res;
+    private TextView molodeccc;
     private ImageButton close;
     private TextView krutaia;
     @Override
@@ -27,11 +28,28 @@ public class molodec extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             krutaia = findViewById(R.id.krutoi);
             close = findViewById(R.id.close_screen);
+            molodeccc = findViewById(R.id.molodecc);
+
             scoree = getIntent().getIntExtra("score", 0);
             res = getIntent().getStringExtra("lowbow");
+
             if (res.equals("low12")) {
+                if (scoree <= 3) {
+                    molodeccc.setText("Спасибо тебе за прохождение квеста! Тебе стоит потренироваться перед следующим прохождением");
+                } else if (scoree > 4 & scoree <= 6) {
+                    molodeccc.setText("Спасибо тебе за прохождение квеста! У тебя неплохой результат!");
+                } else {
+                    molodeccc.setText("Ты огромный молодец! Это прекрасный результат!");
+                }
                 krutaia.setText(String.valueOf(scoree) + " / 10");
             } else if (res.equals("bow12")) {
+                if (scoree <= 5) {
+                    molodeccc.setText("Спасибо тебе за прохождение квеста! Тебе стоит потренироваться перед следующим прохождением");
+                } else if (scoree > 6 & scoree <= 10) {
+                    molodeccc.setText("Спасибо тебе за прохождение квеста! У тебя неплохой результат!");
+                } else {
+                    molodeccc.setText("Ты огромный молодец! Это прекрасный результат!");
+                }
                 krutaia.setText(String.valueOf(scoree) + " / 16");
             }
 
